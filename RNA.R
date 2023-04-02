8e9765867663831e7bf15a5888262f3b194974ad
Claudia Chum <61335977+ClauChum@users.noreply.github.com>
2023-04-02 00:03:28 -0600

    Add files via upload

---
+++ RNA.R
@@ -0,0 +1,16 @@
+library(neuralnet)
+library(tidyverse)
+library(caret)
+data = iris
+valor = createDataPartition(data$Species,p = 0.8, list = F)
+train = data[valor,]
+test = data[-valor,]
+red.neuronal = neuralnet(Species ~ Sepal.Length + Sepal.Width + Petal.Length + Petal.Width, data = train,hidden = c(2,3))
+red.neuronal$act.fct
+plot(red.neuronal)
+prediccion =  predict(red.neuronal, test, type = 'class')
+decodifica.col = apply(prediccion,1, which.max)
+codificado =  data_frame(decodifica.col)
+codificado = mutate(codificado, especie = recode(codificado$decodifica.col,"1"="Setosa: ","2" = "Versicolor: ", "3"="Virginica: "))
+test$Especie = codificado$especie
+print(weights_in_hidden1 <- red.neuronal$weights[[1]])
